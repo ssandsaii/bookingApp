a54324f338549cf1b65daacd48be52ef172fc483
Piyapattae <piyapattae06@gmail.com>
2017-05-24 12:00:05 +0700

    Add files via upload

--- index.android.js
+++ index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class bookingApp extends Component {
+export default class BookingApp extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('bookingApp', () => bookingApp);
+AppRegistry.registerComponent('BookingApp', () => BookingApp);
